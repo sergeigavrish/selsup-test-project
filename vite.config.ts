f4771cc6219f0541c12ed3db504803ac7d1b4464
sergei <sd.gavrish@gmail.com>
2024-03-15 01:49:42 +0100

    ✨ Refactor code for consistency and readability.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import browserslistToEsbuild from 'browserslist-to-esbuild';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import browserslistToEsbuild from "browserslist-to-esbuild";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    target: browserslistToEsbuild()
-  }
-})
+    target: browserslistToEsbuild(),
+  },
+});
